d6a0d02bee67b8d37d75eeb2f2628588c7a6cd6a
Adrián Poza <adripoza23@gmail.com>
2019-11-29 20:59:21 +0100

    commit inicial

---
+++ Scaffold12/app/src/main/java/dadm/scaffold/space/Asteroid.java
@@ -0,0 +1,69 @@
+package dadm.scaffold.space;
+
+import dadm.scaffold.R;
+import dadm.scaffold.engine.GameEngine;
+import dadm.scaffold.engine.ScreenGameObject;
+import dadm.scaffold.engine.Sprite;
+
+public class Asteroid extends Sprite {
+
+    private final GameController gameController;
+
+    private double speed;
+    private double speedX;
+    private double speedY;
+    private double rotationSpeed;
+
+    public Asteroid(GameController gameController, GameEngine gameEngine) {
+        super(gameEngine, R.drawable.a10000);
+        this.speed = 200d * pixelFactor/1000d;
+        this.gameController = gameController;
+    }
+
+    public void init(GameEngine gameEngine) {
+        // They initialize in a [-30, 30] degrees angle
+        double angle = gameEngine.random.nextDouble()*Math.PI/6d-Math.PI/12d;
+        speedX = -speed * Math.cos(angle);
+        speedY = speed * Math.sin(angle);
+        // Asteroids initialize in the central 50% of the screen horizontally
+        positionX =  gameEngine.width;
+        // They initialize outside of the screen horizontally
+        positionY = gameEngine.random.nextInt(gameEngine.height/2)+gameEngine.height/4;
+        rotationSpeed = angle*(180d / Math.PI)/250d; // They rotate 4 times their angle in a second.
+        rotation = gameEngine.random.nextInt(360);
+    }
+
+    @Override
+    public void startGame() {
+    }
+
+    public void removeObject(GameEngine gameEngine) {
+        // Return to the pool
+        gameEngine.removeGameObject(this);
+        gameController.returnToPool(this);
+    }
+
+    @Override
+    public void onUpdate(long elapsedMillis, GameEngine gameEngine) {
+        positionX += speedX * elapsedMillis;
+        positionY += speedY * elapsedMillis;
+        rotation += rotationSpeed * elapsedMillis;
+        if (rotation > 360) {
+            rotation = 0;
+        }
+        else if (rotation < 0) {
+            rotation = 360;
+        }
+        // Check if the sprite goes out of the screen and return it to the pool if so
+        if (positionX < 0) {
+            // Return to the pool
+            gameEngine.removeGameObject(this);
+            gameController.returnToPool(this);
+        }
+    }
+
+    @Override
+    public void onCollision(GameEngine gameEngine, ScreenGameObject otherObject) {
+
+    }
+}
